162780d32208145f6a1e3f5a4f0ae1acbbc16171
Lonny Wong <lonnywong@qq.com>
2022-02-27 12:28:23 +0800

    redirect cli to the python version

--- src/tsz.ts
+++ src/tsz.ts
@@ -4,8 +4,26 @@
  * @license MIT
  */
 
+export {};
+
+const fs = require("fs");
+const { spawnSync } = require("child_process");
+
 console.log(
   "The Node.js version is under development.\n" +
     "Please use the Python version instead.\n" +
-    "GitHub: https://github.com/trzsz/trzsz"
+    "GitHub: https://github.com/trzsz/trzsz\n"
 );
+
+const bin = "/usr/local/bin/tsz";
+
+if (!fs.existsSync(bin)) {
+  process.exit();
+}
+const script = fs.readFileSync(bin, "utf-8");
+if (script.startsWith("#!/usr/bin/env node")) {
+  process.exit();
+}
+
+// redirect to the python version
+spawnSync(bin, process.argv.slice(2), { stdio: "inherit" });
