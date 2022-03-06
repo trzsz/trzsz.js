f2530a37804bb366205b12bb909027facaaa44e9
Lonny Wong <lonnywong@qq.com>
2022-03-06 21:58:22 +0800

    add text progress bar

--- src/progress.ts
+++ src/progress.ts
@@ -8,9 +8,100 @@
 
 import { ProgressCallback } from "./comm";
 
+function getLength(str: string): number {
+  return str.replace(/[\u4e00-\u9fa5]/g, "**").length;
+}
+
+export function getSubstring(str: string, max: number): string {
+  let len = 0;
+  let sub = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 0x4e00 && str.charCodeAt(i) <= 0x9fa5) {
+      if (len + 2 > max) {
+        return sub;
+      }
+      len += 2;
+    } else {
+      if (len + 1 > max) {
+        return sub;
+      }
+      len += 1;
+    }
+    sub += str[i];
+  }
+  return str;
+}
+
+function convertSizeToString(size: number): string {
+  if (!isFinite(size)) {
+    return "NaN";
+  }
+
+  let unit = "B";
+  if (size > 1024) {
+    size = size / 1024;
+    unit = "KB";
+  }
+  if (size > 1024) {
+    size = size / 1024;
+    unit = "MB";
+  }
+  if (size > 1024) {
+    size = size / 1024;
+    unit = "GB";
+  }
+  if (size > 1024) {
+    size = size / 1024;
+    unit = "GB";
+  }
+  if (size > 1024) {
+    size = size / 1024;
+    unit = "TB";
+  }
+
+  let result;
+  if (size >= 100) {
+    result = size.toFixed(0);
+  } else if (size >= 10) {
+    result = size.toFixed(1);
+  } else {
+    result = size.toFixed(2);
+  }
+
+  return result + unit;
+}
+
+function convertTimeToString(seconds: number) {
+  if (!isFinite(seconds)) {
+    return "NaN";
+  }
+
+  let result = "";
+  if (seconds >= 3600) {
+    result += Math.floor(seconds / 3600).toString() + ":";
+    seconds %= 3600;
+  }
+
+  const minute = Math.floor(seconds / 60);
+  result += minute >= 10 ? minute.toString() : "0" + minute.toString();
+  result += ":";
+
+  const second = Math.round(seconds % 60);
+  result += second >= 10 ? second.toString() : "0" + second.toString();
+
+  return result;
+}
+
 export class TextProgressBar implements ProgressCallback {
   private writer: (output: string) => void;
+  private lastUpdateTime: number = 0;
   private columns: number;
+  private fileCount: number;
+  private fileIdx: number;
+  private fileName: string;
+  private fileSize: number;
+  private fileStep: number;
+  private startTime: number;
 
   public constructor(writer: (output: string) => void, columns: number) {
     this.writer = writer;
@@ -21,9 +112,125 @@ export class TextProgressBar implements ProgressCallback {
     this.columns = columns;
   }
 
-  public onNum(num: number) {}
-  public onName(name: string) {}
-  public onSize(size: number) {}
-  public onStep(step: number) {}
-  public onDone(name: string) {}
+  public onNum(num: number) {
+    this.fileCount = num;
+    this.fileIdx = 0;
+  }
+
+  public onName(name: string) {
+    this.fileName = name;
+    this.fileIdx += 1;
+    this.startTime = Date.now();
+  }
+
+  public onSize(size: number) {
+    this.fileSize = size;
+  }
+
+  public onStep(step: number) {
+    this.fileStep = step;
+    this.showProgress();
+  }
+
+  private showProgress() {
+    const now = Date.now();
+    if (now - this.lastUpdateTime < 200) {
+      return;
+    }
+    this.lastUpdateTime = now;
+
+    const percentage = Math.round((this.fileStep * 100) / this.fileSize).toString() + "%";
+    const total = convertSizeToString(this.fileStep);
+    const speed = convertSizeToString((this.fileStep * 1000) / (now - this.startTime)) + "/s";
+    const leftTime = ((this.fileSize - this.fileStep) * (now - this.startTime)) / this.fileStep / 1000;
+    const eta = convertTimeToString(leftTime) + " ETA";
+
+    const progressText = this.getProgressText(percentage, total, speed, eta);
+    this.writer("\r" + progressText);
+  }
+
+  private getProgressText(percentage: string, total: string, speed: string, eta: string) {
+    const barMinLength = 24;
+    let left = this.fileCount > 1 ? `(${this.fileIdx}/${this.fileCount}) ${this.fileName}` : this.fileName;
+    let leftLength = getLength(left);
+    let right = ` ${percentage} | ${total} | ${speed} | ${eta}`;
+
+    do {
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      if (leftLength > 47) {
+        left = getSubstring(left, 47) + "...";
+        leftLength = getLength(left);
+      }
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      if (leftLength > 37) {
+        left = getSubstring(left, 37) + "...";
+        leftLength = getLength(left);
+      }
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      right = ` ${percentage} | ${speed} | ${eta}`;
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      if (leftLength > 27) {
+        left = getSubstring(left, 27) + "...";
+        leftLength = getLength(left);
+      }
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      right = ` ${percentage} | ${eta}`;
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      right = ` ${percentage}`;
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      if (leftLength > 17) {
+        left = getSubstring(left, 17) + "...";
+        leftLength = getLength(left);
+      }
+
+      if (this.columns - leftLength - right.length >= barMinLength) {
+        break;
+      }
+      left = "";
+      leftLength = 0;
+    } while (false);
+
+    let barLength = this.columns - right.length;
+    if (leftLength > 0) {
+      barLength -= leftLength + 1;
+      left += " ";
+    }
+
+    const bar = this.getProgressBar(barLength);
+    return (left + bar + right).trim();
+  }
+
+  private getProgressBar(len: number) {
+    if (len < 12) {
+      return "";
+    }
+    const total = len - 2;
+    const complete = Math.round((total * this.fileStep) / this.fileSize);
+    return "[\u001b[36m" + "\u2588".repeat(complete) + "\u2591".repeat(total - complete) + "\u001b[0m]";
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public onDone(name: string) {
+    this.writer("\r");
+  }
 }
---
+++ test/progress.test.ts
@@ -0,0 +1,238 @@
+/**
+ * trzsz: https://github.com/trzsz/trzsz.js
+ * Copyright(c) 2022 Lonny Wong <lonnywong@qq.com>
+ * @license MIT
+ */
+
+import { TextProgressBar, getSubstring } from "../src/progress";
+
+/* eslint-disable require-jsdoc */
+
+function outputLength(str: string): number {
+  return str
+    .replace("\r", "")
+    .replace(/\u001b\[\d+m/g, "")
+    .replace(/[\u4e00-\u9fa5]/g, "**").length;
+}
+
+let dateNowSpy;
+
+beforeEach(() => {
+  dateNowSpy = jest.spyOn(Date, "now");
+});
+
+afterEach(() => {
+  dateNowSpy.mockRestore();
+});
+
+test("progress bar NaN speed and eta", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564135000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 100);
+  tgb.onNum(1);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(100);
+  tgb.onStep(0);
+  expect(writer.mock.calls.length).toBe(1);
+  expect(dateNowSpy.mock.calls.length).toBe(2);
+  expect(writer.mock.calls[0][0]).toContain("中文😀test.txt [");
+  expect(writer.mock.calls[0][0]).toContain("] 0% | 0.00B | NaN/s | NaN ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(100);
+});
+
+test("progress bar having speed and eta", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564135100);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 100);
+  tgb.onNum(1);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(100);
+  tgb.onStep(1);
+  expect(writer.mock.calls.length).toBe(1);
+  expect(dateNowSpy.mock.calls.length).toBe(2);
+  expect(writer.mock.calls[0][0]).toContain("中文😀test.txt [");
+  expect(writer.mock.calls[0][0]).toContain("] 1% | 1.00B | 10.0B/s | 00:10 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(100);
+});
+
+test("progress bar ouput once only", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564135001).mockReturnValueOnce(1646564135099);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 100);
+  tgb.onNum(1);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(100);
+  tgb.onStep(1);
+  tgb.onStep(2);
+  expect(writer.mock.calls.length).toBe(1);
+  expect(dateNowSpy.mock.calls.length).toBe(3);
+  expect(writer.mock.calls[0][0]).toContain("中文😀test.txt [");
+  expect(writer.mock.calls[0][0]).toContain("] 1% | 1.00B | 1000B/s | 00:00 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(100);
+});
+
+test("progress bar supper fast speed", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 100);
+  tgb.onNum(1);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(1024 * 1024 * 1024 * 1024 * 1024 * 1024);
+  tgb.onStep(10.1 * 1024 * 1024 * 1024 * 1024 * 1024);
+  expect(writer.mock.calls.length).toBe(1);
+  expect(dateNowSpy.mock.calls.length).toBe(2);
+  expect(writer.mock.calls[0][0]).toContain("中文😀test.txt [");
+  expect(writer.mock.calls[0][0]).toContain("] 1% | 10.1TB | 10.1TB/s | 01:40 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(100);
+});
+
+test("progress bar very slow speed", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 100);
+  tgb.onNum(1);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(1024 * 1024);
+  tgb.onStep(1);
+  expect(writer.mock.calls.length).toBe(1);
+  expect(dateNowSpy.mock.calls.length).toBe(2);
+  expect(writer.mock.calls[0][0]).toContain("中文😀test.txt [");
+  expect(writer.mock.calls[0][0]).toContain("] 0% | 1.00B | 1.00B/s | 291:16:15 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(100);
+});
+
+test("progress bar long file name", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000).mockReturnValueOnce(1646564138000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 110);
+  tgb.onNum(1);
+  tgb.onName("中文😀非常长非常长非常长非常长非常长非常长非常长非常长.txt");
+  tgb.onSize(1000);
+  tgb.onStep(100);
+  tgb.setTerminalColumns(100);
+  tgb.onStep(200);
+  expect(writer.mock.calls.length).toBe(2);
+  expect(dateNowSpy.mock.calls.length).toBe(3);
+  expect(writer.mock.calls[0][0]).toContain("中文😀非常长非常长非常长非常长非常长非常长非常... [");
+  expect(writer.mock.calls[0][0]).toContain("] 10% | 100B | 100B/s | 00:09 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(110);
+  expect(writer.mock.calls[1][0]).toContain("中文😀非常长非常长非常长非常长非常长... [");
+  expect(writer.mock.calls[1][0]).toContain("] 20% | 200B | 66.7B/s | 00:12 ETA");
+  expect(outputLength(writer.mock.calls[1][0])).toBe(100);
+});
+
+test("progress bar no total size", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000).mockReturnValueOnce(1646564138000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 90);
+  tgb.onNum(1);
+  tgb.onName("中文😀非常长非常长非常长非常长非常长非常长非常长非常长.txt");
+  tgb.onSize(1000);
+  tgb.onStep(100);
+  tgb.setTerminalColumns(80);
+  tgb.onStep(200);
+  expect(writer.mock.calls.length).toBe(2);
+  expect(dateNowSpy.mock.calls.length).toBe(3);
+  expect(writer.mock.calls[0][0]).toContain("中文😀非常长非常长非常长非常长非常长... [");
+  expect(writer.mock.calls[0][0]).toContain("] 10% | 100B/s | 00:09 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(90);
+  expect(writer.mock.calls[1][0]).toContain("中文😀非常长非常长非常长非... [");
+  expect(writer.mock.calls[1][0]).toContain("] 20% | 66.7B/s | 00:12 ETA");
+  expect(outputLength(writer.mock.calls[1][0])).toBe(80);
+});
+
+test("progress bar no speed and eta", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000).mockReturnValueOnce(1646564138000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 70);
+  tgb.onNum(1);
+  tgb.onName("中文😀longlonglonglonglonglongname.txt");
+  tgb.onSize(1000);
+  tgb.onStep(100);
+  tgb.setTerminalColumns(60);
+  tgb.onStep(200);
+  expect(writer.mock.calls.length).toBe(2);
+  expect(dateNowSpy.mock.calls.length).toBe(3);
+  expect(writer.mock.calls[0][0]).toContain("中文😀longlonglonglonglongl... [");
+  expect(writer.mock.calls[0][0]).toContain("] 10% | 00:09 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(70);
+  expect(writer.mock.calls[1][0]).toContain("中文😀longlonglonglonglongl... [");
+  expect(writer.mock.calls[1][0]).toContain("] 20%");
+  expect(outputLength(writer.mock.calls[1][0])).toBe(60);
+});
+
+test("progress bar no file name", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000).mockReturnValueOnce(1646564138000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 48);
+  tgb.onNum(1);
+  tgb.onName("中文😀llong文件名.txt");
+  tgb.onSize(1000);
+  tgb.onStep(100);
+  tgb.setTerminalColumns(30);
+  tgb.onStep(200);
+  expect(writer.mock.calls.length).toBe(2);
+  expect(dateNowSpy.mock.calls.length).toBe(3);
+  expect(writer.mock.calls[0][0]).toContain("中文😀llong文件名... [");
+  expect(writer.mock.calls[0][0]).toContain("] 10%");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(48);
+  expect(writer.mock.calls[1][0]).not.toContain("中文");
+  expect(writer.mock.calls[1][0]).toContain("] 20%");
+  expect(outputLength(writer.mock.calls[1][0])).toBe(30);
+});
+
+test("progress bar no bar", () => {
+  dateNowSpy.mockReturnValueOnce(1646564135000).mockReturnValueOnce(1646564136000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 10);
+  tgb.onNum(1);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(1000);
+  tgb.onStep(300);
+  expect(writer.mock.calls.length).toBe(1);
+  expect(dateNowSpy.mock.calls.length).toBe(2);
+  expect(writer.mock.calls[0][0].replace("\r", "")).toBe("30%");
+});
+
+test("progress bar multiple files", () => {
+  dateNowSpy
+    .mockReturnValueOnce(1646564135000)
+    .mockReturnValueOnce(1646564136000)
+    .mockReturnValueOnce(1646564137000)
+    .mockReturnValueOnce(1646564139000);
+  const writer = jest.fn();
+  const tgb = new TextProgressBar(writer, 100);
+  tgb.onNum(2);
+  tgb.onName("中文😀test.txt");
+  tgb.onSize(1000);
+  tgb.onStep(100);
+  tgb.onDone("test.txt");
+  tgb.onName("英文😀test.txt");
+  tgb.onSize(2000);
+  tgb.setTerminalColumns(80);
+  tgb.onStep(300);
+  tgb.onDone("test.txt");
+  expect(writer.mock.calls.length).toBe(4);
+  expect(dateNowSpy.mock.calls.length).toBe(4);
+  expect(writer.mock.calls[0][0]).toContain("(1/2) 中文😀test.txt [");
+  expect(writer.mock.calls[0][0]).toContain("] 10% | 100B | 100B/s | 00:09 ETA");
+  expect(outputLength(writer.mock.calls[0][0])).toBe(100);
+  expect(writer.mock.calls[1][0]).toBe("\r");
+  expect(writer.mock.calls[2][0]).toContain("(2/2) 英文😀test.txt [");
+  expect(writer.mock.calls[2][0]).toContain("] 15% | 300B | 150B/s | 00:11 ETA");
+  expect(outputLength(writer.mock.calls[2][0])).toBe(80);
+  expect(writer.mock.calls[3][0]).toBe("\r");
+});
+
+test("get substring with max length", () => {
+  expect(getSubstring("", 10)).toBe("");
+  expect(getSubstring("中文", 1)).toBe("");
+  expect(getSubstring("中文", 2)).toBe("中");
+  expect(getSubstring("😀中", 2)).toBe("😀");
+  expect(getSubstring("😀中", 3)).toBe("😀");
+  expect(getSubstring("😀中", 4)).toBe("😀中");
+  expect(getSubstring("😀q中", 2)).toBe("😀");
+  expect(getSubstring("😀a中", 3)).toBe("😀a");
+  expect(getSubstring("😀a中", 4)).toBe("😀a");
+  expect(getSubstring("😀a中", 5)).toBe("😀a中");
+});
