ec62f9c0ed3b1e1907b88990e1ac5ed051f6df43
zundaren <44863479+zundaren@users.noreply.github.com>
2025-03-06 20:11:50 +0000

    修复node环境报错

--- src/filter.ts
+++ src/filter.ts
@@ -211,13 +211,13 @@ export class TrzszFilter {
     if (this.uploadFilesList || this.isTransferringFiles()) {
       throw new Error("The previous upload has not been completed yet");
     }
-    if (items instanceof DataTransferItemList) {
-      this.uploadFilesList = await parseDataTransferItemList(items as DataTransferItemList);
-    } else if (isArrayOfType(items, "string") && !isRunningInBrowser) {
+    if (!isRunningInBrowser && Array.isArray(items) && isArrayOfType(items, "string")) {
       this.uploadFilesList = await nodefs.checkPathsReadable(items as string[], true);
-    } else {
-      throw new Error("The upload items type is not supported");
-    }
+    } else if (isRunningInBrowser && items instanceof DataTransferItemList) {
+    	 this.uploadFilesList = await parseDataTransferItemList(items as DataTransferItemList);
+  	} else {
+    	  throw new Error("The upload items type is not supported");
+  	}
 
     if (!this.uploadFilesList || !this.uploadFilesList.length) {
       this.uploadFilesList = null;
